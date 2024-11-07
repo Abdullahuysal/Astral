594036486e31dbf0ebbd24060a087ea6363a7b54
Abdullah Uysal <uysalapo1999@gmail.com>
2024-11-07 19:49:46 +0300

    cleared unnecessary files

---
+++ Astral.Membership/src/Host/Astral.Membership.API/Registrars/LoggerRegistrar.cs
@@ -0,0 +1,20 @@
+﻿
+using Serilog;
+
+namespace Astral.Membership.API.Registrars
+{
+    public class LoggerRegistrar : IWebApplicationBuilderRegistrar
+    {
+        public void RegisterServices(WebApplicationBuilder builder)
+        {
+
+            builder.Host.UseSerilog((context, config) =>
+            {
+                config.MinimumLevel.Information()
+                      .WriteTo.Console()
+                      .WriteTo.File("log.txt");
+            });
+
+        }
+    }
+}
